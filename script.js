f56b5abdf5db9aeea82fbf4c184cdf1bad638c18
vedantkherade <152008775+vedantkherade@users.noreply.github.com>
2025-10-08 10:29:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your JS code here. If required.
+
+document.addEventListener("DOMContentLoaded", function(){
+	const username = document.getElementById("username");
+	const password = document.getElementById("password");
+	const checkBox = document.getElementById("checkbox");
+	const submit = document.getElementById("submit");
+	const existing = document.getElementById("existing");
+
+	if(localStorage.getItem("username")){
+		existing.style.display = "block";
+	}
+
+	submit.addEventListener("click", function() {
+		e.preventDefault();
+		alert("Logged in as" + username.value);
+
+		if(checkBox.checked){
+			localStorage.setItem("username", username.value);
+			localStorage.setItem("password", password.value);
+		}else{
+			localStorage.removeItem("username");
+			localStorage.removeItem("password");
+		}
+	});
+});
+
